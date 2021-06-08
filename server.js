c78e4492bb820add0ca836f6730b916c941c09c0
tarunraghav226 <tarun.raghav226@gmail.com>
2021-06-08 19:31:33 +0530

    Added basic node js configuration.

---
+++ server.js
@@ -0,0 +1,11 @@
+const express = require("express");
+
+const app = express();
+
+app.use(express.json());
+
+const port = 3000;
+
+app.listen(port, () => {
+  console.log("SERVER IS RUNNING");
+});
