4f1b42dd663e1c9af89f61023f96bdf7dfbe81d5
tarunraghav226 <tarun.raghav226@gmail.com>
2021-06-09 13:51:40 +0530

    Added dotenv

--- server.js
+++ server.js
@@ -1,7 +1,12 @@
 const express = require("express");
+const morgan = require("morgan");
+const dotenv = require("dotenv");
+
+dotenv.config({ path: "./.env" });
 
 const app = express();
 
+app.use(morgan("dev"));
 app.use(express.json());
 
 const port = 3000;
