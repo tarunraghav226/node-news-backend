043ef1f3b834dfc9f31aff428965c41d7b684aaa
tarunraghav226 <tarun.raghav226@gmail.com>
2021-06-11 19:36:24 +0530

    Added database connection.

--- server.js
+++ server.js
@@ -1,7 +1,26 @@
 const dotenv = require("dotenv");
+const mongoose = require("mongoose");
 const app = require("./app");
 
 dotenv.config({ path: "./.env" });
+
+const DB_URI = process.env.DB_URI.replace(
+  "<PASSWORD>",
+  process.env.DB_PASSWORD
+);
+
+mongoose
+  .connect(DB_URI, {
+    useNewUrlParser: true,
+    useCreateIndex: true,
+    useFindAndModify: false,
+  })
+  .then(() => {
+    console.log("CONNECTED TO DATABASE");
+  })
+  .catch(() => {
+    console.log("CANNOT CONNECT TO DATABASE");
+  });
 
 const port = 3000;
 
