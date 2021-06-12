bf1514e77224bb042848bdd05c5491df7ecfc7a9
tarunraghav226 <tarun.raghav226@gmail.com>
2021-06-12 17:10:06 +0530

    Added middleware to handle global exception

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const morgan = require("morgan");
 const swaggerUi = require("swagger-ui-express");
+const errorHandler = require("./api/dependencies/errorHandler");
 const swaggerFile = require("./swagger_output.json");
 
 const apiRouter = require("./api/api_v1/api");
@@ -13,5 +14,6 @@ swaggerFile.host = "127.0.0.1:3000";
 app.use("/doc", swaggerUi.serve, swaggerUi.setup(swaggerFile));
 
 app.use("/api/v1/", apiRouter);
+app.use(errorHandler);
 
 module.exports = app;
