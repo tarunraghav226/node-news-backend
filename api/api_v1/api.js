c49b423e08260b364ae7c8b2b130f5d21037dac6
tarunraghav226 <tarun.raghav226@gmail.com>
2021-06-13 22:02:25 +0530

    Added user route in api

--- api/api_v1/api.js
+++ api/api_v1/api.js
@@ -2,6 +2,9 @@ const express = require("express");
 
 const apiRouter = express.Router();
 const healthCheck = require("./endpoints/healthCheck");
+const userRouter = require("./endpoints/user");
+
+apiRouter.use("/user", userRouter);
 
 apiRouter.route("/health-check").get(healthCheck);
 
