e8253ec89a403fcb828c05b6a31ade48eeb14974
tarunraghav226 <tarun.raghav226@gmail.com>
2021-06-21 19:23:08 +0530

    Updated endpoint to create user

--- api/api_v1/endpoints/user.js
+++ api/api_v1/endpoints/user.js
@@ -2,10 +2,14 @@
 const express = require("express");
 const catchAsync = require("../../../utils/catchAsync");
 const User = require("../../../models/user");
+const AppError = require("../../../utils/appError");
 
 const userRouter = express.Router();
 
 const userSignUp = catchAsync(async (req, res, next) => {
+  if (req.body.password !== req.body.confirmPassword)
+    next(new AppError("Password and confirm password must be same", 400));
+
   const user = await User.create({
     userName: req.body.userName,
     password: req.body.password,
@@ -13,8 +17,9 @@ const userSignUp = catchAsync(async (req, res, next) => {
     firstName: req.body.firstName,
     lastName: req.body.lastName,
   });
+  user.password = undefined;
   res.status(200).json({
-    ...user._doc,
+    user,
   });
 });
 
