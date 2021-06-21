6f3b12f3659198e55a1ed332f8c74a83899d5ff0
tarunraghav226 <tarun.raghav226@gmail.com>
2021-06-21 18:36:26 +0530

    Removed swagger documentation

--- api/api_v1/endpoints/user.js
+++ api/api_v1/endpoints/user.js
@@ -6,25 +6,6 @@ const User = require("../../../models/user");
 const userRouter = express.Router();
 
 const userSignUp = catchAsync(async (req, res, next) => {
-  /*
-      #swagger.start
-      #swagger.path = '/api/v1/user/signup'
-      #swagger.method = 'post'
-      #swagger.description = 'Create User'
-      #swagger.produces = ['application/json']
-      #swagger.parameters['obj'] = {
-                in: 'body',
-                description: 'Adding new user.',
-                schema: {
-                    $userName: 'string',
-                    $password: 'string',
-                    $confirmPassword: 'string',
-                    $firstName: 'string',
-                    $lastName: 'string'
-                }
-            }
-      #swagger.end
-  */
   const user = await User.create({
     userName: req.body.userName,
     password: req.body.password,
--- app.js
+++ app.js
@@ -1,8 +1,6 @@
 const express = require("express");
 const morgan = require("morgan");
-const swaggerUi = require("swagger-ui-express");
 const errorHandler = require("./api/dependencies/errorHandler");
-const swaggerFile = require("./swagger_output.json");
 
 const apiRouter = require("./api/api_v1/api");
 
@@ -10,8 +8,6 @@ const app = express();
 
 app.use(morgan("dev"));
 app.use(express.json());
-swaggerFile.host = "127.0.0.1:3000";
-app.use("/doc", swaggerUi.serve, swaggerUi.setup(swaggerFile));
 
 app.use("/api/v1/", apiRouter);
 app.use(errorHandler);
