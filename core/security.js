bbb663cfec7ecc486f38ed5e73f71b72f7b20505
tarunraghav226 <tarun.raghav226@gmail.com>
2021-07-04 20:51:25 +0530

    Added function to verify jwt token.

--- core/security.js
+++ core/security.js
@@ -4,3 +4,18 @@ exports.signToken = (userID) =>
   jwt.sign({ userID }, process.env.JWT_SECRET, {
     expiresIn: process.env.JWT_TOKEN_EXPIRE_IN,
   });
+
+exports.verifyToken = async (token) => {
+  const data = {
+    userID: null,
+    err: null,
+  };
+  await jwt.verify(token, process.env.JWT_SECRET, (err, decoded) => {
+    if (err) {
+      data.err = err;
+    } else {
+      data.userID = decoded.userID;
+    }
+  });
+  return data;
+};
