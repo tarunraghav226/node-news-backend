65d34d76d75a81b86deda183d46311175f66026b
tarunraghav226 <tarun.raghav226@gmail.com>
2021-07-12 21:13:24 +0530

    Updated api file.

--- api/api_v1/api.js
+++ api/api_v1/api.js
@@ -3,8 +3,10 @@ const express = require("express");
 const apiRouter = express.Router();
 const healthCheck = require("./endpoints/healthCheck");
 const userRouter = require("./endpoints/user");
+const postRouter = require("./endpoints/post");
 
 apiRouter.use("/user", userRouter);
+apiRouter.use("/post", postRouter);
 
 apiRouter.route("/health-check").get(healthCheck);
 
