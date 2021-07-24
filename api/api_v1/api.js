b8e31952aaf679f8635f9380bead20b5afe1422d
tarunraghav226 <tarun.raghav226@gmail.com>
2021-07-24 20:53:34 +0530

    Fixed bug.

--- api/api_v1/api.js
+++ api/api_v1/api.js
@@ -4,9 +4,11 @@ const apiRouter = express.Router();
 const healthCheck = require("./endpoints/healthCheck");
 const userRouter = require("./endpoints/user");
 const postRouter = require("./endpoints/post");
+const imageRouter = require("./endpoints/image");
 
 apiRouter.use("/user", userRouter);
 apiRouter.use("/post", postRouter);
+apiRouter.use("/image", imageRouter);
 
 apiRouter.route("/health-check").get(healthCheck);
 
