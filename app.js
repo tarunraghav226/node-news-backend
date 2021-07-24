a00e6cd2a5206311be4fe7c486ebe8910c5d38de
tarunraghav226 <tarun.raghav226@gmail.com>
2021-07-24 20:53:43 +0530

    Fixed bug.

--- app.js
+++ app.js
@@ -8,7 +8,7 @@ const app = express();
 
 app.use(morgan("dev"));
 app.use(express.json());
-app.use(express.static("public"));
+app.use(express.static("uploads"));
 
 app.use("/api/v1/", apiRouter);
 app.use(errorHandler);
