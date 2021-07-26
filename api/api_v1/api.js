a3a903729535d25637783740a2b372cf3d99d9e9
tarunraghav226 <tarun.raghav226@gmail.com>
2021-07-26 19:58:24 +0530

    Added comment router.

--- api/api_v1/api.js
+++ api/api_v1/api.js
@@ -5,10 +5,12 @@ const healthCheck = require("./endpoints/healthCheck");
 const userRouter = require("./endpoints/user");
 const postRouter = require("./endpoints/post");
 const imageRouter = require("./endpoints/image");
+const commentRouter = require("./endpoints/comment");
 
 apiRouter.use("/user", userRouter);
 apiRouter.use("/post", postRouter);
 apiRouter.use("/image", imageRouter);
+apiRouter.use("/comment", commentRouter);
 
 apiRouter.route("/health-check").get(healthCheck);
 
