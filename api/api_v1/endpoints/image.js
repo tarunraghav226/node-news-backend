327fc27e1423663414ebd98997d4681b38ad10dd
tarunraghav226 <tarun.raghav226@gmail.com>
2021-08-13 20:34:07 +0530

    Added aws s3 image upload.

--- api/api_v1/endpoints/image.js
+++ api/api_v1/endpoints/image.js
@@ -1,4 +1,6 @@
 const express = require("express");
+const S3 = require("aws-sdk/clients/s3");
+const fs = require("fs");
 
 const upload = require("../../dependencies/imageHandler");
 const catchAsync = require("../../../utils/catchAsync");
@@ -8,8 +10,25 @@ const Image = require("../../../models/image");
 const imageRouter = express.Router();
 
 const uploadImage = catchAsync(async (req, res, next) => {
+  const s3 = new S3({
+    bucketRegion: process.env.AWS_BUCKET_REGION,
+    accessKeyId: process.env.AWS_ACCESS_KEY,
+    secretAccessKey: process.env.AWS_SECRET_KEY,
+  });
+
+  const fileStream = fs.createReadStream(req.file.path);
+
+  const uploadParams = {
+    Bucket: process.env.AWS_BUCKET_NAME,
+    Body: fileStream,
+    Key: req.file.filename,
+    ContentDisposition: "inline",
+    ContentType: req.file.mimetype,
+  };
+  const awsRes = await s3.upload(uploadParams).promise();
+
   const image = await Image.create({
-    imageURL: process.env.API_V1_HOST_NAME + req.file.filename,
+    imageURL: awsRes.Location,
   });
   res.status(200).json(image);
 });
