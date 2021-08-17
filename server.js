fb3a7c7ad3340f3f9b9ac5cea74e6634e1ca6ca8
tarunraghav226 <tarun.raghav226@gmail.com>
2021-08-17 19:50:59 +0530

    Added heroku setup

--- server.js
+++ server.js
@@ -22,7 +22,7 @@ mongoose
     console.log("CANNOT CONNECT TO DATABASE");
   });
 
-const port = 3000;
+const port = process.env.PORT || 3000;
 
 app.listen(port, () => {
   console.log("SERVER IS RUNNING");
